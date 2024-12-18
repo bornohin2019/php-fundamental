94dc3bfabf06c3857fa8c1d63386b05653cb0a12
Md-Badhon-Sheikh <badhon0854@gmail.com>
2024-12-19 01:07:07 +0600

    added more info

--- login_registration_with_database/home.php
+++ login_registration_with_database/home.php
@@ -1,8 +1,9 @@
 <?php
 session_start();
-if(!isset($_SESSION['mySession'])){
-    header('location:login.php');
-}
+
+// if(!isset($_SESSION['mySession'])){
+//     header('location:login.php');
+// }
 
 
 ?>
