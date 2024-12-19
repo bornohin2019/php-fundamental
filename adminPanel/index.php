093f756098cc31d5df761bfef38bf1ae1bbc6aa5
Md-Badhon-Sheikh <badhon0854@gmail.com>
2024-12-19 13:02:46 +0600

    added admin file

---
+++ adminPanel/index.php
@@ -0,0 +1,12 @@
+        <?php 
+        include("./pages/common_pages/header.php");
+        ?>
+        <!-- sidebar part start  -->
+        <?php 
+        include("./pages/common_pages/sidebar.php");
+        ?>
+        <!--end::Sidebar--> <!--begin::App Main-->
+        
+       <?php 
+       include("./pages/common_pages/footer.php");
+       ?>
